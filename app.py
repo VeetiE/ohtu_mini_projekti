3dad0993b02aec5662988b9ce4551b057cd42e10
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-11-23 13:16:15 +0200

    added form for book information and extracted the form data

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
-
+from flask import render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Toimiiko tämä teillä!"
+     return render_template("base.html")
+
+@app.route("/submitReferenceInformation/", methods=["POST"])
+def result():
+    referenceType = request.form["referenceType"]
+    bookName = request.form["title"]
+    author = request.form["author"]
+    published = request.form["published"]
+    print(referenceType)
+    print(bookName)
+    print(author)
+    print(published)
+    return render_template("base.html")
+
