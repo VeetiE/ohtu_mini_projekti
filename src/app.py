9b5d92f7adfa3762e8305398e7592f1395e27456
Matin kannettava <matti.kahkonen99@gmail.com>
2022-12-01 11:34:51 +0200

    app.py pylint fixes

--- src/app.py
+++ src/app.py
@@ -7,27 +7,27 @@ from references.reference_bank import ReferenceBank
 
 app = Flask(__name__)
 
-referenceStorage = ReferenceBank()
+reference_storage = ReferenceBank()
 
 
 @app.route("/")
 def index():
-    return render_template("print_doc.html", references=list(referenceStorage.reference_bank.values()))
+    return render_template("print_doc.html", references=list(reference_storage.reference_bank.values()))
 
 
 @app.route("/submitReferenceInformation/", methods=["POST"])
 def result():
-    referenceType = request.form["referenceType"]
-    bookAuthor = request.form["author"]
-    bookName = request.form["title"]
-    bookPublisher = request.form["publisher"]
-    bookAddress = request.form["address"]
-    bookDate = request.form["published"]
+    #reference_type = request.form["referenceType"]
+    book_author = request.form["author"]
+    book_name = request.form["title"]
+    book_publisher = request.form["publisher"]
+    book_address = request.form["address"]
+    book_date = request.form["published"]
 
-    book = Book("book", bookAuthor, bookName,
-                bookPublisher, bookAddress, bookDate)
-    referenceStorage.add_reference(book)
-    print(referenceStorage.reference_bank)
+    book = Book("book", book_author, book_name,
+                book_publisher, book_address, book_date)
+    reference_storage.add_reference(book)
+    print(reference_storage.reference_bank)
 
     return redirect("/")
 
@@ -39,6 +39,6 @@ def referenceForm():
 
 @app.route("/printBibtex")
 def printBibtex():
-    generator = BibtexGenerator(referenceStorage.reference_bank)
+    generator = BibtexGenerator(reference_storage.reference_bank)
     bibtex_list = generator.make_bibtex_list()
     return render_template("print_bibtex.html", bibtexlist=bibtex_list)
