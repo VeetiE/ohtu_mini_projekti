8003ba30a6356a5e91ba6ca064ad33c185084833
Kaapo Kivioja <kaapo.kivioja@helsinki.fi>
2022-12-01 11:50:39 +0200

    Fixed pylint

--- src/references/book.py
+++ src/references/book.py
@@ -5,8 +5,8 @@ class Book:
     Create a book object
     '''
 
-    def __init__(self, type: str, author: str, title: str, publisher: str, address: str, year: int):
-        self.type = type
+    def __init__(self, ref_type, author: str, title: str, publisher: str, address: str, year: int):
+        self.ref_type = ref_type
         self.author = author
         self.title = title
         self.publisher = publisher
@@ -14,4 +14,4 @@ class Book:
         self.year = year
 
     def __str__(self):
-        return f"{self.type}, {self.author}, {self.title}, {self.publisher}, {self.address}, {self.year}"
+        return f"{self.ref_type}, {self.author}, {self.title}, {self.publisher}, {self.address}, {self.year}"
