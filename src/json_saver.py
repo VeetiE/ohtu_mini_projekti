0df563b76f3d5cfbc19ab5db95839ead2c09955f
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-12-07 12:55:28 +0200

    save_dictionary_to_json now takes filename as argument

--- src/json_saver.py
+++ src/json_saver.py
@@ -6,14 +6,14 @@ class Converter(json.JSONEncoder):
     def default(self, o):
         return o.__dict__
 
-def save_dictionary_to_json(dictionary):
+def save_dictionary_to_json(dictionary, filename):
     json_obj = json.dumps(dictionary, indent=4, cls=Converter)
 
-    with open("testi.json", "w", encoding="utf-8") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         file.write(json_obj)
         file.close()
 
-    with open("testi.json", "r", encoding="utf-8") as openFile:
+    with open(filename, "r", encoding="utf-8") as openFile:
         data = json.load(openFile)
         openFile.close()
     print(data)
