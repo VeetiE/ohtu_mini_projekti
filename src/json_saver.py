d509f5f7d426ddaa7c3931315c151a65954ee3ee
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-12-07 12:43:41 +0200

    removed unused function and removed testi.json

--- src/json_saver.py
+++ src/json_saver.py
@@ -6,24 +6,6 @@ class Converter(json.JSONEncoder):
     def default(self, o):
         return o.__dict__
 
-
-def save_array_to_json(in_list):
-    json_books_list = []
-    for book in in_list:
-        json_books_list.append(book.__dict__)
-
-    json_obj = json.dumps(json_books_list, indent=4)
-
-    with open("testi.json", "w", encoding="utf-8") as file:
-        file.write(json_obj)
-        file.close()
-
-    with open("testi.json", "r", encoding="utf-8") as openFile:
-        data = json.load(openFile)
-        openFile.close()
-    print(data)
-
-
 def save_dictionary_to_json(dictionary):
     json_obj = json.dumps(dictionary, indent=4, cls=Converter)
 
